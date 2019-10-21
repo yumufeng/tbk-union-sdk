f586d857e45e1028bd01f2af22c51ef1d0218861
shaojuntan <tsj100@qq.com>
2019-10-21 11:18:51 +0800

    swoole支持长空间

--- src/Tools/Helpers.php
+++ src/Tools/Helpers.php
@@ -34,7 +34,7 @@ class Helpers
                 $queryUrl = $urlsInfo['path'];
                 $domain = $urlsInfo['host'];
                 $port = $urlsInfo['scheme'] = 'https' ? 443 : 80;
-                $chan = new \Chan(1);
+                $chan = new \Swoole\Coroutine\Channel(1);
                 go(function () use ($chan, $domain, $queryUrl, $header, $port, $post_data) {
                     $cli = new \Swoole\Coroutine\Http\Client($domain, $port, $port == 443 ? true : false);
                     $cli->setHeaders($header);
