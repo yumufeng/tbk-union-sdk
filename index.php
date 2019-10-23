e33648b4f758ed3a7248a93b021c468bcfa3496d
Storm <admin@yumufeng.com>
2019-10-24 00:35:36 +0800

    淘宝客-推广者-图文内容效果数据bug修复

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 require './vendor/autoload.php';
 header("content-type: application/json;charset=UTF-8");
 
@@ -7,19 +8,14 @@ $config = [
     'appkey' => '25085171',
     'secretKey' => 'f66bd449316c3e7c958b83e821d2bb6b',
     'pid' => 'mm_29563340_122900348_29874650260', //默认推广位
-    'session' => '',//授权接口（sc类的接口）需要带上
-    'sandbox' => false,
+    'session' => '61011267a0f75da4f1753e1ac2a4d58fa111a3a1a6ede79602540452',//授权接口（sc类的接口）需要带上
+    'sandbox' => true,
 ];
 
 $client = new \TaobaoUnionSdk\TbkFatory($config);
 
-$result = $client->tools->spreadGet([
-    [
-        'url' => 'https://uland.taobao.com/quan/detail?sellerId=4051125533&activityId=8a18f770d12849f2bfcdc31c418e8a5c'
-    ],
-    [
-        'url' => 'http://temai.taobao.com'
-    ]
+$result = $client->content->effect([
+    'content_id' => '9440815'
 ]);
 
 if ($result == false) {
